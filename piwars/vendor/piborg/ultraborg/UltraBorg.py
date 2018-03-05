e420adf7235bb8fbb0536b1f7be5cb696a622ae1
Tim Golden <mail@timgolden.me.uk>
2018-03-05 07:29:06 +0000

    Dodge the encoding bullet by switching the superscript 2 to a standard one

--- piwars/vendor/piborg/ultraborg/UltraBorg.py
+++ piwars/vendor/piborg/ultraborg/UltraBorg.py
@@ -9,7 +9,7 @@ UB = UltraBorg.UltraBorg()
 UB.Init()
 # User code here, use UB to control the board
 
-Multiple boards can be used when configured with different I²C addresses by creating multiple instances, e.g.
+Multiple boards can be used when configured with different I2C addresses by creating multiple instances, e.g.
 import UltraBorg
 UB1 = UltraBorg.UltraBorg()
 UB2 = UltraBorg.UltraBorg()
@@ -101,11 +101,11 @@ def ScanForUltraBorg(busNumber = 1):
     """
 ScanForUltraBorg([busNumber])
 
-Scans the I²C bus for a UltraBorg boards and returns a list of all usable addresses
-The busNumber if supplied is which I²C bus to scan, 0 for Rev 1 boards, 1 for Rev 2 boards, if not supplied the default is 1
+Scans the I2C bus for a UltraBorg boards and returns a list of all usable addresses
+The busNumber if supplied is which I2C bus to scan, 0 for Rev 1 boards, 1 for Rev 2 boards, if not supplied the default is 1
     """
     found = []
-    print('Scanning I²C bus #%d' % (busNumber))
+    print('Scanning I2C bus #%d' % (busNumber))
     bus = UltraBorg()
     for address in range(0x03, 0x78, 1):
         try:
@@ -136,25 +136,25 @@ def SetNewAddress(newAddress, oldAddress = -1, busNumber = 1):
     """
 SetNewAddress(newAddress, [oldAddress], [busNumber])
 
-Scans the I²C bus for the first UltraBorg and sets it to a new I2C address
+Scans the I2C bus for the first UltraBorg and sets it to a new I2C address
 If oldAddress is supplied it will change the address of the board at that address rather than scanning the bus
-The busNumber if supplied is which I²C bus to scan, 0 for Rev 1 boards, 1 for Rev 2 boards, if not supplied the default is 1
-Warning, this new I²C address will still be used after resetting the power on the device
+The busNumber if supplied is which I2C bus to scan, 0 for Rev 1 boards, 1 for Rev 2 boards, if not supplied the default is 1
+Warning, this new I2C address will still be used after resetting the power on the device
     """
     if newAddress < 0x03:
-        print('Error, I²C addresses below 3 (0x03) are reserved, use an address between 3 (0x03) and 119 (0x77)')
+        print('Error, I2C addresses below 3 (0x03) are reserved, use an address between 3 (0x03) and 119 (0x77)')
         return
     elif newAddress > 0x77:
-        print('Error, I²C addresses above 119 (0x77) are reserved, use an address between 3 (0x03) and 119 (0x77)')
+        print('Error, I2C addresses above 119 (0x77) are reserved, use an address between 3 (0x03) and 119 (0x77)')
         return
     if oldAddress < 0x0:
         found = ScanForUltraBorg(busNumber)
         if len(found) < 1:
-            print('No UltraBorg boards found, cannot set a new I²C address!')
+            print('No UltraBorg boards found, cannot set a new I2C address!')
             return
         else:
             oldAddress = found[0]
-    print('Changing I²C address from %02X to %02X (bus #%d)' % (oldAddress, newAddress, busNumber))
+    print('Changing I2C address from %02X to %02X (bus #%d)' % (oldAddress, newAddress, busNumber))
     bus = UltraBorg()
     bus.InitBusOnly(busNumber, oldAddress)
     try:
@@ -197,9 +197,9 @@ Warning, this new I²C address will still be used after resetting the power on th
             foundChip = False
             print('Missing UltraBorg at %02X' % (newAddress))
     if foundChip:
-        print('New I²C address of %02X set successfully' % (newAddress))
+        print('New I2C address of %02X set successfully' % (newAddress))
     else:
-        print('Failed to set new I²C address...')
+        print('Failed to set new I2C address...')
 
 
 # Class used to control UltraBorg
@@ -207,16 +207,16 @@ class UltraBorg:
     """
 This module is designed to communicate with the UltraBorg
 
-busNumber               I²C bus on which the UltraBorg is attached (Rev 1 is bus 0, Rev 2 is bus 1)
-bus                     the smbus object used to talk to the I²C bus
-i2cAddress              The I²C address of the UltraBorg chip to control
+busNumber               I2C bus on which the UltraBorg is attached (Rev 1 is bus 0, Rev 2 is bus 1)
+bus                     the smbus object used to talk to the I2C bus
+i2cAddress              The I2C address of the UltraBorg chip to control
 foundChip               True if the UltraBorg chip can be seen, False otherwise
 printFunction           Function reference to call when printing text, if None "print" is used
     """
 
     # Shared values used by this class
     busNumber               = 1                 # Check here for Rev 1 vs Rev 2 and select the correct bus
-    i2cAddress              = I2C_ID_SERVO_USM  # I²C address, override for a different address
+    i2cAddress              = I2C_ID_SERVO_USM  # I2C address, override for a different address
     foundChip               = False
     printFunction           = None
     i2cWrite                = None

80b0decb83e513c36d39b428dde2621b163af48a
Tim Golden <mail@timgolden.me.uk>
2018-04-19 09:03:40 +0000

    Switch the vertical axis (because we wired the motors backwards)

--- piwars/tbJoystick.py
+++ piwars/tbJoystick.py
@@ -6,7 +6,7 @@ import time
 import os
 import sys
 import pygame
-import ThunderBorg
+from .vendor.piborg.thunderborg import ThunderBorg
 
 # Re-direct our output to standard error, we need to ignore standard out to hide some nasty print statements from pygame
 sys.stdout = sys.stderr
@@ -18,13 +18,13 @@ TB.Init()
 if not TB.foundChip:
     boards = ThunderBorg.ScanForThunderBorg()
     if len(boards) == 0:
-        print 'No ThunderBorg found, check you are attached :)'
+        print('No ThunderBorg found, check you are attached :)')
     else:
-        print 'No ThunderBorg at address %02X, but we did find boards:' % (TB.i2cAddress)
+        print('No ThunderBorg at address %02X, but we did find boards:' % (TB.i2cAddress))
         for board in boards:
-            print '    %02X (%d)' % (board, board)
-        print 'If you need to change the I²C address change the setup line so it is correct, e.g.'
-        print 'TB.i2cAddress = 0x%02X' % (boards[0])
+            print('    %02X (%d)' % (board, board))
+        print('If you need to change the I²C address change the setup line so it is correct, e.g.')
+        print('TB.i2cAddress = 0x%02X' % (boards[0]))
     sys.exit()
 # Ensure the communications failsafe has been enabled!
 failsafe = False
@@ -34,12 +34,12 @@ for i in range(5):
     if failsafe:
         break
 if not failsafe:
-    print 'Board %02X failed to report in failsafe mode!' % (TB.i2cAddress)
+    print('Board %02X failed to report in failsafe mode!' % (TB.i2cAddress))
     sys.exit()
 
 # Settings for the joystick
 axisUpDown = 1                          # Joystick axis to read for up / down position
-axisUpDownInverted = False              # Set this to True if up and down appear to be swapped
+axisUpDownInverted = True              # Set this to True if up and down appear to be swapped
 axisLeftRight = 2                       # Joystick axis to read for left / right position
 axisLeftRightInverted = False           # Set this to True if left and right appear to be swapped
 buttonSlow = 8                          # Joystick button number for driving slowly whilst held (L2)
@@ -60,13 +60,13 @@ else:
 # Show battery monitoring settings
 battMin, battMax = TB.GetBatteryMonitoringLimits()
 battCurrent = TB.GetBatteryReading()
-print 'Battery monitoring settings:'
-print '    Minimum  (red)     %02.2f V' % (battMin)
-print '    Half-way (yellow)  %02.2f V' % ((battMin + battMax) / 2)
-print '    Maximum  (green)   %02.2f V' % (battMax)
-print
-print '    Current voltage    %02.2f V' % (battCurrent)
-print
+print('Battery monitoring settings:')
+print('    Minimum  (red)     %02.2f V' % (battMin))
+print('    Half-way (yellow)  %02.2f V' % ((battMin + battMax) / 2))
+print('    Maximum  (green)   %02.2f V' % (battMax))
+print()
+print('    Current voltage    %02.2f V' % (battCurrent))
+print()
 
 # Setup pygame and wait for the joystick to become available
 TB.MotorsOff()
@@ -75,7 +75,7 @@ TB.SetLeds(0,0,1)
 os.environ["SDL_VIDEODRIVER"] = "dummy" # Removes the need to have a GUI window
 pygame.init()
 #pygame.display.set_mode((1,1))
-print 'Waiting for joystick... (press CTRL+C to abort)'
+print('Waiting for joystick... (press CTRL+C to abort)')
 while True:
     try:
         try:
@@ -97,16 +97,16 @@ while True:
             time.sleep(0.1)
     except KeyboardInterrupt:
         # CTRL+C exit, give up
-        print '\nUser aborted'
+        print('\nUser aborted')
         TB.SetCommsFailsafe(False)
         TB.SetLeds(0,0,0)
         sys.exit()
-print 'Joystick found'
+print('Joystick found')
 joystick.init()
 TB.SetLedShowBattery(True)
 ledBatteryMode = True
 try:
-    print 'Press CTRL+C to quit'
+    print('Press CTRL+C to quit')
     driveLeft = 0.0
     driveRight = 0.0
     running = True
